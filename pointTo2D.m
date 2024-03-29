8c9f182cbdfb446c023d09e3c19e05adf578a370
Ethan Davis <exd5220@psu.edu>
2024-03-28 20:34:36 -0400

    some progerss on part 1

--- pointTo2D.m
+++ pointTo2D.m
@@ -1,4 +1,4 @@
-function [x_out,y_out] = pointTo2D(x_in,y_in,z_in,param)
+function [x_out,y_out] = pointTo2D(world_pos,param)
 %pointTo2D: takes 3d points x,y, and z and the camera parameters and
 %returns x,y coordinates in which the point will apear on camera
 
@@ -11,6 +11,22 @@ for i=1:3
 end
 
 %construct R matrix
-R=
+R=zeros(4);
+R(4,4) = 1;
+for i=1:3
+    %Zhat
+    R(3,i)=world_pos(i)-cam.position(i);
+end
+%Xhat
+R(1,1:3)=cross([0,0,1],R(3,1:3));
+%Yhat
+R(2,1:3)=cross(R(3,1:3),R(1,1:3));
+
+%make perspective matrix
+Pers=zeros(3,4);
+Pers(3,3)=1;
+Pers(1,1)=cam.foclen;
+Pers(2,2)=cam.foclen;
+
 
 end
