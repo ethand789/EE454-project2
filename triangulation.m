3c7cdfa21585b536f337770b120797ae82c5eb74
Emmanuel <ezh5373@psu.edu>
2024-03-31 22:39:15 -0400

    started 3.2

---
+++ triangulation.m
@@ -0,0 +1,14 @@
+function [point1_out, point2_out] = triangulation(point2,point2)
+% takes two 2D points and returns a recovered 3D point by
+% performing triangulation.
+
+% get camera parameters
+cam1 = load("Project2DataFiles\Parameters_V1.mat").Parameters;
+cam2 = load("Project2DataFiles\Parameters_V2.mat").Parameters;
+
+% compute location of camera
+
+
+
+
+end
